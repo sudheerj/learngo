fe3093146fd58725ded929a454d6c393b79be1f2
sudheerj <jsudheer50@gmail.com>
2020-09-07 23:25:40 +0530

    Add If statement

--- 01-beginner/07-if/If.go
+++ 01-beginner/07-if/If.go
@@ -1,1 +1,16 @@
 package main
+
+import "fmt"
+
+func oddOrEven(x int) {
+	if v := x%2; v == 0 {
+		fmt.Printf("The reminder is %v. Number %v is an even number\n", v,x)
+	} else {
+		fmt.Printf("The reminder is %v. Number %v is an odd number\n", v,x)
+	}
+}
+
+func main() {
+		oddOrEven(10)
+		oddOrEven(5)
+}
